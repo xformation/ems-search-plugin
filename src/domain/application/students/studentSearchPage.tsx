fe21d6e5b5dc69690d915a8d12bcbdfa200e2b56
manojkrjoshi <44704973+manojkrjoshi@users.noreply.github.com>
2020-04-21 12:20:04 +0530

    css name corrected

--- src/domain/application/students/studentSearchPage.tsx
+++ src/domain/application/students/studentSearchPage.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { parse } from 'query-string';
 // import { graphql, QueryProps, MutationFunc, compose } from 'react-apollo';
 // import {TabContent, TabPane, Nav, NavItem, NavLink} from 'reactstrap';
-import "../../../css/studentSearchApp.css";
+import '../../../css/StudentSearchApp.css';
 import { studentServices } from '../_services/students.service';
 import wsCmsBackendServiceClient from '../../../wsCmsBackendServiceClient';
 
